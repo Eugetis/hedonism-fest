c52df4caff94c339bb443ab4c857b66a1dde7646
unknown <marahin1992@ya.ru>
2023-08-22 12:03:28 +0300

    created main-content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './pages/index.css';
 
 console.log('hello');
+
+const sliderLine = document.querySelector('.gallery__slider-line');
+const sliderDots = document.querySelectorAll('.gallery__slider-radio');
+const sliderImages = document.querySelectorAll('.gallery__image');
+        
+// Переменные    
+let sliderCount = 0;
+let sliderWidth;
+
+console.log(sliderImages);
+    
+
+window.addEventListener('resize', showSlide);
+
+function showSlide() {
+   sliderWidth = document.querySelector('.gallery__slider').offsetWidth;
+   if (window.innerWidth < 768) {
+    sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
+
+   rollSlider();
+    } else {
+        sliderLine.style.transform = `translateX(0)`;
+        sliderLine.style.width = '100%';
+    }
+}
+
+showSlide();
+
+function rollSlider() {
+    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+function thisSlide(index) {
+    sliderDots.forEach(item => item.classList.remove('gallery__slider-radio_active'));
+    sliderDots[index].classList.add('gallery__slider-radio_active');
+}
+sliderDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        sliderCount = index;
+        rollSlider();
+        thisSlide(sliderCount);
+    })
+})
