c99e0b4b885de607a1523ee362a6729b3faa5ea7
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-08-23 12:33:21 +0300

    Модалка v0.0.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './scss/styles.scss';
+const modalButton = document.querySelector('.modal__button');
+const modal = document.querySelector('.modal');
+const modalContainer = document.querySelector('.modal__container');
+
+modalButton.addEventListener('click', (event) => {
+  modal.classList.add('modal_opened');
+  modalContainer.classList.add('modal__container_opened');
+})
 
 console.log('hello');
