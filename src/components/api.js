6ce27fae2b543595a3f8ebca54109036532b6a91
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-08-29 22:16:23 +0300

    Добавил 2 функции, getCards, getCities -> для получения данных с API

--- src/components/api.js
+++ src/components/api.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 // ТУТ БУДУТ ЗАПРОСЫ К ФЕЙКОВОМУ API
 
 // Если вдруг кому-то нужно что-то дописать в этом файле, помимо основного ответственного за эту функциональность,
@@ -8,7 +9,33 @@
 
 // Никита - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+const getCards = async () => {
+  try {
+    const response = await axios.get('https://run.mocky.io/v3/e782198a-ac8f-4097-a5fc-83db97ed561b', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
 
+    return response.data;
+  } catch (e) {
+    console.log(e.message, + ' getCards')
+  }
+}
+
+const getCities = async () => {
+  try {
+    const response = await axios.get('https://run.mocky.io/v3/7cdf8973-35bd-40dd-b6e8-3d95ac446576', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    return response.data;
+  } catch (e) {
+    console.log(e.message, ' getCities')
+  }
+}
 
 // Никита -> end!
 
@@ -47,3 +74,8 @@
 // Евгений -> end!
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+module.exports = {
+  getCards,
+  getCities
+}
