4515cf3f2b1a0767a29d7799e7c0055f9c9adec4
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-08-30 11:56:50 +0300

    Добавил добавление/удаления лайков, localstorage

--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios';
 // ТУТ БУДУТ ЗАПРОСЫ К ФЕЙКОВОМУ API
 
 // Если вдруг кому-то нужно что-то дописать в этом файле, помимо основного ответственного за эту функциональность,
@@ -9,7 +9,7 @@ const axios = require('axios');
 
 // Никита - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-const getCards = async () => {
+export const getCards = async () => {
   try {
     const response = await axios.get('https://run.mocky.io/v3/e782198a-ac8f-4097-a5fc-83db97ed561b', {
       headers: {
@@ -23,7 +23,7 @@ const getCards = async () => {
   }
 }
 
-const getCities = async () => {
+export const getCities = async () => {
   try {
     const response = await axios.get('https://run.mocky.io/v3/7cdf8973-35bd-40dd-b6e8-3d95ac446576', {
       headers: {
@@ -74,8 +74,3 @@ const getCities = async () => {
 // Евгений -> end!
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-module.exports = {
-  getCards,
-  getCities
-}
