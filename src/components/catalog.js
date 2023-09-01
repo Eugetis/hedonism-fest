f2b6f1ae687422b8d65c5b7a8a7825d0762b8b69
dmitry.razumov.nb <dmitry.razumov@novsu.ru>
2023-09-01 12:23:55 +0300

    feat: реализована фильтрация по таг кнопкам и прокинут вариант рендера карточек по ее результатам

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,5 +1,5 @@
 import {getCards, getCardById} from './api';
-import {prepareCard, modalCreate, modalHandler, addCard} from './event';
+import {prepareCard, modalCreate, modalHandler, addCard, removeCards, addCardsToLocalStorage, getCardsFromLocaleStorage} from './event';
 import {tabSwitcher, mapContainer, listContainer} from './constants';
 
 // ФУНКЦИОНАЛ СТРАНИЦЫ "КАТАЛОГ"
@@ -18,9 +18,37 @@ export const catalogController = async (section, template) => {
   setTabSwitchEventListener();
   // Dmitry -> end!
   const cards = await getCards();
+  // Dmitry
+  // сохраняем все карточки сразу при первой отрисовке в local Storage
+  // вешаем на все кнопки таг фильтров слушателей, которые вызывают универсалный контроллер фильтрации
+  addCardsToLocalStorage(cards);
+  setTagsFilterEventListener();
+  // Dmitry -> end!
   const preparedCards = prepareCard(cards, template);
   return addCard(preparedCards, section);
 }
+
+// Dmitry
+const arrayValues = (array) => {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      result.push(array[i][j]);
+    }
+  }
+
+  return result;
+}
+// дергаем эту функцию как только обработали кнопки фильтрации для перерисовки карточек
+const renderCatalog = (section, template, filteredCards) => {
+  removeCards(section);
+  const cards = arrayValues(Object.values(filteredCards));
+  console.log('filteredCards -> ' + cards);
+  const preparedCards = prepareCard({cards}, template);
+  return addCard(preparedCards, section);
+}
+// Dmitry -> end!
 
 // получение (нужен запрос к API в api.js) и вставка карточек мероприятий в грид (рендер - в events.js)
 
@@ -40,7 +68,6 @@ export const generalCardController = async (section, template) => {
   const preparedCards = prepareCard(cards, template);
   return addCard(preparedCards, section);
 }
-
 
 // Никита -> end!
 
@@ -87,7 +114,6 @@ const initMap = () => {
 // в первый раз - создаем карту (создаем её "ленивым подходом", асинхронно)
 // в последующие - просто обновляем её отображение
 const showMap = () => {
-    //console.log(mapContainer.querySelector('.catalog__map-container'));
     if (myMap) {
       myMap.container.fitToViewport();
     } else {
@@ -95,7 +121,6 @@ const showMap = () => {
       ymaps.ready(myMap);
       updateCity();
     }
-    //console.log(myMap);
 }
 
 // Запрос координат в апи карты по имени
@@ -161,8 +186,123 @@ function setTabSwitchEventListener() {
     });
   }
 
+// ========================
 // работа фильтров
 // также нужны скрипты и слушатели на кнопки в секции с карточками на главной странице (это связано, поэтому написал тут)
+let filteredCards = {};
+
+// функция делающая отфильтрованные карточки, вызывается контроллером таг фильтров после обработки нажатия на кнопку фильтра
+function getFilteredCards(activeTags) {
+  const cards = getCardsFromLocaleStorage();
+  const keys = activeTags.keys();
+  let filteredCardsByDay = {};
+  filteredCards = {};
+  activeTags['dayTags'].forEach(day => {
+    let res = {};
+    if (day.localeCompare('все') === 0) {
+      res = cards;
+    } else {
+      res = cards.filter(card => {
+        const dateContent = card.date.split(' ');
+        const date = `${dateContent[0].padStart(2,'0')} ${dateContent[1].substring(0, 3)}.`
+        const dayContent = day.split(', ');
+        return dayContent.includes(date);
+      });
+    }
+    console.log(res);
+    if (res.length) {
+      filteredCardsByDay = res;
+    }
+    return res;
+  })
+
+  activeTags['eventTags'].forEach(event => {
+    let res = {};
+    if (event.localeCompare('все') === 0) {
+      res = filteredCardsByDay;
+    } else {
+      res = filteredCardsByDay.filter(card => {
+        return event.includes(card.type)
+      });
+    }
+    console.log(res);
+    if (res.length) {
+      filteredCards[event] = res;
+    }
+    return res;
+  })
+  console.log(filteredCards);
+  return filteredCards;
+}
+
+const activeTags = [];
+// инициализация слушателей кнопок таг фильтров
+const setTagsFilterEventListener = () => {
+  if (document.querySelector('.page_id_catalog')) {
+    const filtersGroups = document.querySelectorAll('.catalog__filters-group');
+    filtersGroups.forEach(filtersGroup  => {
+      const subtitle = filtersGroup.closest('.catalog__section').querySelector('.catalog__subtitle');
+      const filterButtons = filtersGroup.querySelectorAll('.tag-filter')
+      const filterButtonsList = Array.from(filterButtons);
+      activeTags[subtitle.id] = new Set;
+      filterButtonsList.forEach(button => {
+        button.addEventListener('click', evt => filtersClickController(evt, activeTags));
+        if (button.classList.contains('tag-filter_type_all')) {
+          activeTags[subtitle.id].add(button.textContent);
+        }
+      });
+    });
+  }
+}
+
+// контроллер обработки нажатия на любую кнопку таг фильтров
+// в заключении дергает функцию обновления информации на странице (список\карта)
+export const filtersClickController = (evt, activeTags) => {
+  console.log(activeTags);
+
+  const target = evt.target;
+  const button = target.closest('.tag-filter');
+  const filtersGroup = button.closest('.catalog__filters-group')
+  const buttonAll = filtersGroup.querySelector('.tag-filter_type_all');
+
+  const subtitle = filtersGroup.closest('.catalog__section').querySelector('.catalog__subtitle');
+  if (button.textContent === buttonAll.textContent) {
+    buttonAll.classList.add('tag-filter_type_selected');
+    activeTags[subtitle.id].add(button.textContent);
+
+    const tagIconList = Array.from(filtersGroup.querySelectorAll('.tag-filter__icon'));
+    tagIconList.forEach(tagIcon => {
+      tagIcon.classList.add('tag-filter__icon_type_hidden');
+      tagIcon.closest('.tag-filter').classList.remove('tag-filter_type_selected');
+      activeTags[subtitle.id].delete(tagIcon.closest('.tag-filter').textContent);
+    });
+  } else {
+    if (!activeTags[subtitle.id].delete(button.textContent)) {
+      activeTags[subtitle.id].add(button.textContent);
+    }
+    activeTags[subtitle.id].delete(buttonAll.textContent);
+    buttonAll.classList.remove('tag-filter_type_selected');
+
+    button.querySelector('.tag-filter__icon').classList.toggle('tag-filter__icon_type_hidden');
+    button.classList.toggle('tag-filter_type_selected');
+  }
+
+  console.log('activeTags -> ');
+  console.log(activeTags);
+
+  renderCardController(activeTags);
+
+}
+
+// получаем набор отфильтрованных карточек и делаем рендеринг
+export const renderCardController =  (activeTags) => {
+
+  const filteredCards = getFilteredCards(activeTags);
+
+  const cardGridSection = document.querySelector('.cards_type_grid');
+  const cardTemplate = cardGridSection.querySelector('#card').content;
+  renderCatalog(cardGridSection, cardTemplate, filteredCards);
+}
 
 // инициализация яндекс карты при клике на нужный таб и все скрипты по теме карты
 // (скрипты открытия модалок напишет Андрей в modal.js)
